a0203b539174b25f165ce51ac89277ab0bb66932
Manthan <manthan.asher@gmail.com>
2022-12-09 23:40:49 +0530

    Final commit

--- app/src/androidTest/java/androidsamples/java/tictactoe/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/androidsamples/java/tictactoe/ExampleInstrumentedTest.java
@@ -1,14 +1,29 @@
 package androidsamples.java.tictactoe;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
 import android.content.Context;
 
-import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.fragment.app.FragmentFactory;
+import androidx.fragment.app.testing.FragmentScenario;
+import androidx.navigation.Navigation;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.espresso.accessibility.AccessibilityChecks;
+import androidx.navigation.testing.TestNavHostController;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
+
+import java.util.Objects;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -17,10 +32,38 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("androidsamples.java.tictactoe", appContext.getPackageName());
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    /**
+     * Accessibility check.
+     */
+    @BeforeClass
+    public static void enableAccessibilityChecks() {
+        AccessibilityChecks.enable();
     }
+
+
+//    @Test
+//    public void testNavigationToEntryListFragment() {
+//        // Create a TestNavHostController
+//        TestNavHostController navController = new TestNavHostController(
+//                ApplicationProvider.getApplicationContext());
+//
+//        FragmentScenario<DashboardFragment> entryDetailsFragmentFragmentScenario
+//                = FragmentScenario.launchInContainer(DashboardFragment.class, null, R.style.Theme_AppCompat, (FragmentFactory) null);
+//
+//        entryDetailsFragmentFragmentScenario.onFragment(fragment -> {
+//            // Set the graph on the TestNavHostController
+//            navController.setGraph(R.navigation.nav_graph);
+//
+//            // Make the NavController available via the findNavController() APIs
+//            Navigation.setViewNavController(fragment.requireView(), navController);
+//        });
+//
+//        // Verify that performing a click changes the NavController's state
+//        onView(withId(R.id.fab_new_game)).perform(click());
+//        assertThat(Objects.requireNonNull(navController.getCurrentDestination()).getId(), is(R.id.gameFragment));
+//
+//    }
 }
